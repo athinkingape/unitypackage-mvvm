6f8e3b74624fa034efebfbcaaede09709dd3b2c1
Tymur Koshel <timurkoshel@gmail.com>
2024-05-07 14:44:41 -0700

    ObservableModel basic implementation

--- Runtime/Models/ObservableModel.cs
+++ Runtime/Models/ObservableModel.cs
@@ -1,4 +1,36 @@
 namespace MVVM.Models
 {
-    public class ObservableModel<T> : BaseObservable<T> { }
+    public class ObservableModel<T> : BaseObservable<T>, IObservableValue<T>
+        where T : BaseObservable<T>
+    {
+        public T Value { get; private set; }
+
+        public ObservableModel(T value)
+        {
+            Setup(value);
+        }
+
+        public void Setup(T value)
+        {
+            if (Value != null && Value.Equals(value))
+            {
+                return;
+            }
+
+            if (Value != null)
+            {
+                Value.RemoveObservation(OnValueUpdated);
+            }
+            
+            Value = value;
+            Value.Observe(OnValueUpdated);
+            
+            NotifyObservers(value);
+        }
+
+        private void OnValueUpdated(T obj)
+        {
+            NotifyObservers(obj);
+        }
+    }
 }
