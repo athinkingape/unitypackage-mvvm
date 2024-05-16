7576da4cddbfdbdc14aaad81b14b6a9eec49a11c
Tymur Koshel <timurkoshel@gmail.com>
2024-05-15 20:41:16 -0700

    View and ViewModel binding

---
+++ Runtime/Bindings/ViewModelBinding.cs
@@ -0,0 +1,28 @@
+using MVVM.Models;
+using MVVM.ViewModels;
+using MVVM.Views;
+
+namespace MVVM.Bindings.Base
+{
+    public class ViewModelBinding<TView, TViewModel> : BaseValueBinding<TViewModel>
+        where TViewModel : BaseViewModel
+        where TView : BaseView<TViewModel>
+    {
+        private readonly TView _view;
+        
+        public ViewModelBinding(IObservableValue<TViewModel> observableValue, TView view) : base(observableValue)
+        {
+            _view = view;
+        }
+
+        public ViewModelBinding(TView view, TViewModel value) : base(value)
+        {
+            _view = view;
+        }
+
+        protected override void OnUpdate(TViewModel value)
+        {
+            _view.Setup(value);
+        }
+    }
+}
